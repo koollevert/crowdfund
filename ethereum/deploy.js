25baea696d1a9591c9fe25a2d65cbcdac79663a0
leVert <selmamathis15@gmail.com>
2024-08-14 19:35:16 +0300

    deploy script

---
+++ ethereum/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const {Web3} = require('web3');
+// const { abi, bytecode } = require('./build/Inbox.json');
+// const compiledFactory = require('../build/CampaignFactory.json');
+const compiledFactory = require('C:/Users/joshu/Desktop/crowdfund/ethereum/build/CampaignFactory.json');
+
+
+const provider = new HDWalletProvider(
+    'saddle connect tumble beyond impose siren spin brain crumble chief grief dice',
+    'https://sepolia.infura.io/v3/b3be9208b48349fe9f9aa63ac4cf8b68'
+);
+provider.engine._blockTracker._pollingInterval = 8000; // Set polling interval to 8 seconds
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from Account', accounts[0]);
+
+    const result = await new web3.eth.Contract(compiledFactory.interface)
+        .deploy({ data: bytecode })
+        .send({ gas: '1000000', from: accounts[0] });
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();  // Stop the provider engine
+};
+
+deploy();
