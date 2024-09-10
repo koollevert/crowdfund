1df495ab7e29b31f8831e284c1b178a60712cc71
leVert <selmamathis15@gmail.com>
2024-09-10 12:44:03 +0300

    fetch deployed contracts

--- ethereum/factory.ts
+++ ethereum/factory.ts
@@ -1,9 +1,9 @@
 import web3 from './web3';
 import CampaignFcatory from './build/CampaignFactory.json';
 
-const instance = new web3.eth.Contract(
+const factory = new web3.eth.Contract(
     CampaignFcatory.interface,
     '0x3E76aadd570De10C8517DB71E112215834EA8FA5'
 );
 
-export default instance;
+export default factory;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+'use client'
 import Image from "next/image";
+import factory from "../../ethereum/factory"; 
+import { Component} from "react";
 
-export default function Home() {
-  return(
-    <div>home page</div>
-  ); 
+// export default function Home() {
+//   return(
+//     <div>home page</div>
+//   ); 
+// }
+
+export default class CampaignIndex extends Component{
+  async componentDidCatch(){
+    const campaigns=await factory.methods.getDeployedCampigns().call();
+    console.log(campaigns);
+  }
+  render(){
+    return <div>Campaigns Index</div>
+  }
+
 }
