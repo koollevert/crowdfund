8279e42196bb7f5a13c8d4a4e7b0f54df3755576
leVert <selmamathis15@gmail.com>
2024-09-16 14:33:03 +0300

    summary translation layer

--- src/app/campaigns/[address]/page.tsx
+++ src/app/campaigns/[address]/page.tsx
@@ -6,9 +6,20 @@ interface CampaignShowPageProps {
   }
   
 export default async function CampaignShow({params}:CampaignShowPageProps){
-    const campaign=Campaign(params.address);
-    const summary= await campaign.methods.getSummary().call();
-    console.log(summary);
+    async function fetchData() {
+        const campaign = Campaign(params.address);
+        const summary = await campaign.methods.getSummary().call();
+    
+        //translation layer
+        return {
+          minimumContribution: summary[0],
+          balance: summary[1],
+          requestCount: summary[2],
+          approversCount: summary[3],
+          manager: summary[4],
+        };
+      }
+
     return(
         <div>Show</div>
     )
