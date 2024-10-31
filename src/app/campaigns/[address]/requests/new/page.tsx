caa9e5010dbe23e79e1f0aa8ce857b67c55cc6b4
leVert <selmamathis15@gmail.com>
2024-10-31 18:53:52 +0300

    better new request form useform, form state

--- src/app/campaigns/[address]/requests/new/page.tsx
+++ src/app/campaigns/[address]/requests/new/page.tsx
@@ -3,23 +3,30 @@
 import { useState } from 'react';
 import web3 from '../../../../../../ethereum/web3';
 import campaign from '../../../../../../ethereum/campaign';
+import { Input } from '@nextui-org/react';
+import { useForm } from 'react-hook-form';
+import FormButton from '@/components/form-button';
 
-export default function NewRequest({ params }) {
-    const [description, setDescription] = useState('');
-    const [value, setValue] = useState('');
-    const [recipient, setRecipient] = useState('');
+interface RequestProps {
+    params: { address: string };
+}
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+export default function NewRequest({ params }: RequestProps) {
+    const { register, handleSubmit, formState } = useForm();
+    const { errors } = formState;
+
+    const onSubmit = async ({data}: any) => {
         const campaignInstance = campaign(params.address);
 
         try {
             const accounts = await web3.eth.getAccounts();
-            await campaignInstance.methods.createRequest(
-                description,
-                web3.utils.toWei(value, 'ether'),
-                recipient
-            ).send({ from: accounts[0] });
+            await campaignInstance.methods
+                .createRequest(
+                    data.description,
+                    web3.utils.toWei(data.value, 'ether'),
+                    data.recipient
+                )
+                .send({ from: accounts[0] });
             // Handle success
         } catch (err) {
             console.error(err);
@@ -28,29 +35,47 @@ export default function NewRequest({ params }) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-                <label>Description</label>
-                <input
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
+                <Input
+                    {...register('description', { required: 'Description is required' })}
+                    isInvalid={!!errors.description}
+                    errorMessage={errors.description?.message as string || ''}
+                    label="Description"
+                    labelPlacement="outside"
+                    placeholder="Description"
                 />
             </div>
+
             <div>
-                <label>Value in Ether</label>
-                <input
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
+                <Input
+                    {...register('value', { required: 'Value in Ether is required' })}
+                    isInvalid={!!errors.value}
+                    errorMessage={errors.value?.message as string || ''}
+                    label="Value in Ether"
+                    labelPlacement="outside"
+                    placeholder="Value"
                 />
             </div>
+
             <div>
-                <label>Recipient Address</label>
-                <input
-                    value={recipient}
-                    onChange={(e) => setRecipient(e.target.value)}
+                <Input
+                    {...register('recipient', { required: 'Recipient address is required' })}
+                    isInvalid={!!errors.recipient}
+                    errorMessage={errors.recipient?.message as string || ''}
+                    label="Recipient Address"
+                    labelPlacement="outside"
+                    placeholder="Recipient Address"
                 />
             </div>
-            <button type="submit">Create Request</button>
+
+            {errors._form ? (
+                <div className="rounded p-2 bg-red-200 border border-red-400">
+                    {(errors._form.message as string) || ''}
+                </div>
+            ) : null}
+
+            <FormButton>Create Request</FormButton>
         </form>
     );
 }
